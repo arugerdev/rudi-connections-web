8d2f34c58335b8b93eea20c1b80c55973bb3aa92
arugerdev <alvaroch250@gmail.com>
2025-02-20 11:09:02 +0100

    Finish Fix config button

--- components/buttons/ConfigureDeviceButtonModal.tsx
+++ components/buttons/ConfigureDeviceButtonModal.tsx
@@ -18,9 +18,7 @@ export const ConfigureDeviceButtonModal = ({ device, resetList = () => { } }: {
 }) => {
 
     return (
-        <Button onClick={() => {
-            window.location.href = `http://${device.config?.tailscale?.website ?? 'rud1.es'}?key=${device?.secret_key ?? 'error'}&device=${device.key ?? 'error'}`
-        }} isDisabled={!device.config?.tailscale?.website || device.config?.tailscale?.website == '' || device.status !== 'running'} color="secondary" className="cursor-pointer" variant="flat">
+        <Button as={Link} isDisabled={!device.config?.tailscale?.website || device.config?.tailscale?.website == '' || device.status !== 'running'} rel="noopener noreferrer" target="_blank" href={`http://${device.config?.tailscale?.website ?? 'rud1.es'}?key=${device?.secret_key ?? 'error'}&device=${device.key ?? 'error'}`} color="secondary" className="cursor-pointer" variant="flat">
             Configurar
         </Button >
     )
